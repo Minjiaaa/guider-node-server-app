a3ec1abba0d3c0a82805ac715d183a30947e3c1a
angel <63884758+AngelG8@users.noreply.github.com>
2023-06-24 17:33:43 -0500

    updated allowed origins

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
 );
 
 app.use((req, res, next) => {
-    const allowedOrigins = ["http://localhost:3000", "https://", "https://main--taupe-churros-eca8b0.netlify.app/"];
+    const allowedOrigins = ["http://localhost:3000", "https://", "https://taupe-churros-eca8b0.netlify.app/", "https://main--taupe-churros-eca8b0.netlify.app/", "https://angel--taupe-churros-eca8b0.netlify.app/"];
     const origin = req.headers.origin;
 
     if (allowedOrigins.includes(origin)) {

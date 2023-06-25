6e91fdd6c6f4dd2b6c6b1ee7e3cdfb91eef0938c
ZeguiJiang <monarchgui@gmail.com>
2023-06-25 13:59:28 -0700

    user shema changed

--- users/users-schema.js
+++ users/users-schema.js
@@ -1,8 +1,23 @@
 import mongoose from "mongoose";
 const usersSchema = new mongoose.Schema({
+    // username: { type: String, required: true, unique: true },
+    // password: { type: String, required: true },
+    // firstName: String,
+    // lastName: String,
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    firstName: String,
-    lastName: String,
+    firstName: { type: String, default: "None" },
+    lastName: { type: String, default: "None" },
+    email: { type: String, default: "None" },
+    phone: { type: String, default: "000-000-0000" },
+    role: {
+        type: String,
+        default: "Viewer",
+    },
+    dob: Date,
+    created: { type: Date, default: Date.now },
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'usersModel' }],
+    followeing: [{ type: mongoose.Schema.Types.ObjectId, ref: 'usersModel' }],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'tuitsModel' }],
 }, { collection: "users" });
 export default usersSchema;
